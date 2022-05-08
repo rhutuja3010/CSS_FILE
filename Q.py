4f4bc500a73a53439c0152ea205dc965b96ce87f
rhutuja3010 <91016307+rhutuja3010@users.noreply.github.com>
2022-05-08 20:56:33 +0530

    Add files via upload

---
+++ Q.py
@@ -0,0 +1,36 @@
+# x=3
+# if x>2 or x<5 and x==6:
+#     print("ok")
+# else:
+#     print("no")
+
+
+# a=5
+# if a<4 and a<5 or a==5:
+#     print("Rhutuja")
+# else:
+#     print("Not output")
+
+
+# car_color="black"
+# if 3>2:
+#     if car_color=="blue":
+#         print("You Rock")
+#     else:
+#         print("Boring")
+
+
+# My_name="Rhutuja"
+# if 7>6:
+#     if My_name=="Rhutuja":
+#         print("Yes")
+#     else:
+#         print("No")
+
+
+# print(1 in [[1],2,3])
+
+# l=[[q] for q in (0,4) for q in (0,4)]
+# print(l)
+
+
